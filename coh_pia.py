875c0f69284105961e1f291ee62897d89223cae9
PedroStrabeli <pedrostrabeli@gmail.com>
2017-05-10 09:58:06 -0300

    passed 80% co-piah

--- coh_pia.py
+++ coh_pia.py
@@ -1,19 +1,20 @@
 import re
 
+###################### GIVEN METHODS ##############################
 def le_assinatura():
     '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
 
-    # wal = float(input("Entre o tamanho medio de palavra:"))
-    # ttr = float(input("Entre a relação Type-Token:"))
-    # hlr = float(input("Entre a Razão Hapax Legomana:"))
-    # sal = float(input("Entre o tamanho médio de sentença:"))
-    # sac = float(input("Entre a complexidade média da sentença:"))
-    # pal = float(input("Entre o tamanho medio de frase:"))
+    wal = float(input("Entre o tamanho medio de palavra:"))
+    ttr = float(input("Entre a relação Type-Token:"))
+    hlr = float(input("Entre a Razão Hapax Legomana:"))
+    sal = float(input("Entre o tamanho médio de sentença:"))
+    sac = float(input("Entre a complexidade média da sentença:"))
+    pal = float(input("Entre o tamanho medio de frase:"))
 
 
-    return [4.79, 0.72, 0.56, 80.5, 2.5, 31.6] #mockado
-    # return [wal, ttr, hlr, sal, sac, pal]
+    # return [4.79, 0.72, 0.56, 80.5, 2.5, 31.6] #mockado
+    return [wal, ttr, hlr, sal, sac, pal]
 
 def le_textos():
     i = 1
@@ -25,6 +26,9 @@ def le_textos():
         texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
 
     return textos
+    # return ['Navegadores antigos tinham uma frase gloriosa:"Navegar é preciso; viver não é preciso". Quero para mim o espírito [d]esta frase, transformada a forma para a casar como eu sou: Viver não é necessário; o que é necessário é criar. Não conto gozar a minha vida; nem em gozá-la penso. Só quero torná-la grande,ainda que para isso tenha de ser o meu corpo e a (minha alma) a lenha desse fogo. Só quero torná-la de toda a humanidade;ainda que para isso tenha de a perder como minha. Cada vez mais assim penso.Cada vez mais ponho da essência anímica do meu sangueo propósito impessoal de engrandecer a pátria e contribuirpara a evolução da humanidade.É a forma que em mim tomou o misticismo da nossa Raça.', 
+    # 'Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.',
+    # 'NOSSA alegria diante dum sistema metafisico, nossa satisfação em presença duma construção do pensamento, em que a organização espiritual do mundo se mostra num conjunto lógico, coerente a harmônico, sempre dependem eminentemente da estética; têm a mesma origem que o prazer, que a alta satisfação, sempre serena afinal, que a atividade artística nos proporciona quando cria a ordem e a forma a nos permite abranger com a vista o caos da vida, dando-lhe transparência.']
 
 def separa_sentencas(texto):
     '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
@@ -72,26 +76,88 @@ def n_palavras_diferentes(lista_palavras):
 def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
     partialSum=0
-    while i in range(len(as_a)):
+    for i in range(len(as_a)):
         partialSum += abs(as_a[i]-as_b[i])
     return partialSum/len(as_a)
 
 def calcula_assinatura(texto):
     '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
-    pass
+    return [calculatewal(texto), calculatettr(texto), calculatehlr(texto), calculatesal(texto), calculatesac(texto), calculatepal(texto)]
 
 def avalia_textos(textos, ass_cp):
+    calculados = CalculateSignatures(textos)
+    mapper = dict()
+    for i in range(len(calculados)):
+        mapper[i+1]=compara_assinatura(calculados[i], ass_cp)
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+    return min(mapper, key = mapper.get)
 
-def CalculateParameters(texts):
+
+################## COMPLEMENTARY METHODS ####################
+
+def TrimWord(word):
+    newWord = word;
+    for i in word:
+            if i in [',', '.','?','!',':',';']:
+                newWord = newWord.replace(i, '')
+    return newWord
+
+def calculatewal(texto):
+    # retorna tamanho medio de palavra
+    wordList = separa_palavras(texto)
+    total = 0
+    for word in wordList:
+        total += len(TrimWord(word).strip())
+
+    return  total/len(wordList)
+
+def calculatettr(texto):
+    # retorna relação Type-Token
+    wordList = separa_palavras(texto)
+    
+    return n_palavras_diferentes(wordList)/len(wordList)
+
+def calculatehlr(texto):
+    # retorna Razão Hapax Legomana
+    wordList = separa_palavras(texto)
+    return n_palavras_unicas(wordList)/len(wordList)
+
+def calculatesal(texto):
+    # retorna tamanho médio de sentença
+    sentenceList = separa_sentencas(texto)
+    total = 0
+    for s in sentenceList:
+        total += len(s.strip())
+    return total/len(texto)
+
+def calculatesac(texto):
+    # retorna complexidade média da sentença
+    return len(separa_frases(texto))/len(separa_sentencas(texto))
+
+def calculatepal(texto):
+    # retorna tamanho medio de frase
+    phraseList = separa_frases(texto)
+    total = 0
+    for word in phraseList:
+        total += len(word.strip())
+
+    return  total/len(phraseList)
+
+def CalculateSignatures(texts):
+    chaves = []
+    for text in texts:
+        chaves.append(calcula_assinatura(text))
+    return chaves
+
+########################################################################
+
 
 
 def main():
     params = le_assinatura()
     textos = le_textos()
 
-    calculados = calculateParameters(textos)
-    print(params, textos)
+    infectado = avalia_textos(textos, params)
+    print("O autor do texto", infectado, "está infectado com COH-PIAH")
 
 main()
